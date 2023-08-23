f2577e66f5828ea1ae9bc89a65e1e4aff47ca00d
MHoussam <mohammadosta1999@gmail.com>
2023-08-24 01:44:28 +0300

    Client <>Created the Login Page & the Navbar in the Home Page.

---
+++ client/src/Pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from "../../Components/ui/LoginForm";
+const Login = () => {
+    //const [user, setUser] = useState([]);
+
+  return (
+    <div>
+        <LoginForm />
+    </div>
+  )
+}
+
+export default Login;
